cf9ffd9dc4ca3365a8aa31418296369e82be5f64
Jan Deketelaere <jan.deketelaere@hotmail.com>
2017-12-07 13:44:07 +0100

    Removed unused Extensions class

--- SimpleWebApi/SimpleWebApi/Infrastructure/Extensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace SimpleWebApi.Infrastructure
-{
-    public static class Extensions
-    {
-        public static bool IsSuccessStatusCode(this HttpStatusCode statusCode) => ((int)statusCode >= 200) && ((int)statusCode <= 299);
-    }
-}
